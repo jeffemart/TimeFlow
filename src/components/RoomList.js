b3b8adddd3a79894a5339842e3fdafc55010cd0f
jeffemart <jefferson.github@gmail.com>
2023-09-08 15:20:17 -0300

    link basico de paginas

---
+++ src/components/RoomList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function RoomList() {
+  return (
+    <div>
+      <h2>Lista de Salas Disponíveis</h2>
+      {/* Renderize a lista de salas aqui */}
+    </div>
+  );
+}
+
+export default RoomList;
---
+++ src/components/RoomSchedule.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function RoomSchedule() {
+  return (
+    <div>
+      <h2>Agendamentos de Sala</h2>
+      {/* Lista de agendamentos de sala aqui */}
+    </div>
+  );
+}
+
+export default RoomSchedule;
