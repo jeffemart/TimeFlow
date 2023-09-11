5cecb01440b7b03141c448f311c70a3e863fb05e
jeffemart <jefferson.github@gmail.com>
2023-09-11 11:47:43 -0300

    adicionando formulário de login basico

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import UserLogin from './components/Login/Login';
-import Signup from './components/Signup/Signup';
+import Blog from  './pages/Blog';
+import Home from './pages/Home';
 
 import './App.css';
 
@@ -10,8 +11,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<UserLogin />}></Route>
-        <Route path='/' element={<Signup />}></Route>
+        <Route path='/login' element={<UserLogin />}></Route>
+        <Route path='/' element={<Home />} />
+        <Route path='/blog/:id' element={<Blog />} />
       </Routes>
     </BrowserRouter>
   );
